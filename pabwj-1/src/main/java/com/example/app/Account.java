71531c117071cc27b682ca7f46192e44f81e506f
xShyQx <93669491+xShyQx@users.noreply.github.com>
2024-03-15 10:38:23 +0100

    Dodalem implementacje interfejsu Bank w pabwj-1

--- pabwj-1/src/main/java/com/example/app/Account.java
+++ pabwj-1/src/main/java/com/example/app/Account.java
@@ -1,6 +1,14 @@
 package com.example.app;
 
-public record Account(
-  String username,
-  String email
-) {}
+import lombok.Data;
+import lombok.NonNull;
+
+import java.math.BigDecimal;
+
+@Data
+public class Account {
+    @NonNull private Long id;
+    @NonNull private String name;
+    @NonNull private String address;
+    @NonNull private BigDecimal balance;
+}
--- pabwj-1/src/main/java/com/example/app/BankImpl.java
+++ pabwj-1/src/main/java/com/example/app/BankImpl.java
@@ -1,46 +1,75 @@
 package com.example.app;
 
+import java.lang.invoke.LambdaMetafactory;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 public class BankImpl implements Bank {
+  private final List<Account> accounts;
+
+  private Long getNextId() {
+    if(accounts.isEmpty()) return 1L;
+    return accounts.get(accounts.size()-1).getId();
+  }
+
+  private Account findAccountById(Long id) {
+    Optional<Account> fetchedAccount = accounts
+            .stream()
+            .filter(account -> Objects.equals(account.getId(), id))
+            .findFirst();
+    if(fetchedAccount.isEmpty()) throw new AccountIdException();
+    return fetchedAccount.get();
+  }
+
   public BankImpl() {
-    
+    accounts = new ArrayList<>();
   }
 
   @Override
   public Long createAccount(String name, String address) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'createAccount'");
+    Long existingAccountId = findAccount(name, address);
+    if(existingAccountId != null) return existingAccountId;
+
+    Account newAccount = new Account(getNextId(), name, address, new BigDecimal("0.0"));
+    accounts.add(newAccount);
+    return newAccount.getId();
   }
 
   @Override
   public Long findAccount(String name, String address) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'findAccount'");
+    Optional<Account> existingAccount = accounts
+            .stream()
+            .filter(account -> Objects.equals(account.getName(), name) && Objects.equals(account.getAddress(), address))
+            .findFirst();
+
+    return existingAccount.map(Account::getId).orElse(null);
   }
 
   @Override
   public void deposit(Long id, BigDecimal amount) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'deposit'");
+    Account account = findAccountById(id);
+    account.setBalance(account.getBalance().add(amount));
   }
 
   @Override
   public BigDecimal getBalance(Long id) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getBalance'");
+    Account account = findAccountById(id);
+    return account.getBalance();
   }
 
   @Override
   public void withdraw(Long id, BigDecimal amount) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
+    Account account = findAccountById(id);
+    if(account.getBalance().compareTo(amount) < 0) throw new InsufficientFundsException();
+    account.setBalance(account.getBalance().subtract(amount));
   }
 
   @Override
   public void transfer(Long idSource, Long idDestination, BigDecimal amount) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'transfer'");
+    withdraw(idSource, amount);
+    deposit(idDestination, amount);
   }
-
 }
